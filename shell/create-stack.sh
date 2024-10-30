5dd395d0ae6fcbf026e7bb033384d4ab68dfd32b
Simone Sensidoni <simone.sensidoni@bitapp.it>
2024-10-30 14:51:41 +0100

    update stack to use aws profile

--- shell/create-stack.sh
+++ shell/create-stack.sh
@@ -8,12 +8,13 @@ export S3_ARTIFACT_BUCKET_NAME=$STACK_NAME-$UNIQUE_IDENTIFIER
 export DATA_LOADER_S3_KEY="agent/lambda/data-loader/loader_deployment_package.zip"
 export LAMBDA_HANDLER_S3_KEY="agent/lambda/agent-handler/agent_deployment_package.zip"
 export LEX_BOT_S3_KEY="agent/bot/lex.zip"
+export AWS_PROFILE="aws-bedrock-langchain-agent-saltasu"
 
 echo "STACK_NAME: $STACK_NAME"
 echo "S3_ARTIFACT_BUCKET_NAME: $S3_ARTIFACT_BUCKET_NAME"
 
-aws s3 mb s3://$S3_ARTIFACT_BUCKET_NAME --region $AWS_REGION
-aws s3 cp ../agent/ s3://$S3_ARTIFACT_BUCKET_NAME/agent/ --region $AWS_REGION --recursive --exclude ".DS_Store" --exclude "*/.DS_Store"
+aws s3 mb s3://$S3_ARTIFACT_BUCKET_NAME --region $AWS_REGION  --profile=$AWS_PROFILE
+aws s3 cp ../agent/ s3://$S3_ARTIFACT_BUCKET_NAME/agent/ --region $AWS_REGION --recursive --exclude ".DS_Store" --exclude "*/.DS_Store" --profile=$AWS_PROFILE
 
 export BEDROCK_LANGCHAIN_PDFRW_LAYER_ARN=$(aws lambda publish-layer-version \
     --layer-name bedrock-langchain-pdfrw \
@@ -22,6 +23,7 @@ export BEDROCK_LANGCHAIN_PDFRW_LAYER_ARN=$(aws lambda publish-layer-version \
     --content S3Bucket=$S3_ARTIFACT_BUCKET_NAME,S3Key=agent/lambda/lambda-layers/bedrock-langchain-pdfrw.zip \
     --compatible-runtimes python3.11 \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query LayerVersionArn --output text)
 
 export CFNRESPONSE_LAYER_ARN=$(aws lambda publish-layer-version \
@@ -31,10 +33,11 @@ export CFNRESPONSE_LAYER_ARN=$(aws lambda publish-layer-version \
     --content S3Bucket=$S3_ARTIFACT_BUCKET_NAME,S3Key=agent/lambda/lambda-layers/cfnresponse-layer.zip \
     --compatible-runtimes python3.11 \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query LayerVersionArn --output text)
 
 export GITHUB_TOKEN_SECRET_NAME=$(aws secretsmanager create-secret --name $STACK_NAME-git-pat \
---secret-string $GITHUB_PAT --region $AWS_REGION --query Name --output text)
+--secret-string $GITHUB_PAT --region $AWS_REGION --profile=$AWS_PROFILE --query Name --output text)
 
 aws cloudformation create-stack \
 --stack-name $STACK_NAME \
@@ -50,39 +53,45 @@ ParameterKey=GitHubTokenSecretName,ParameterValue=$GITHUB_TOKEN_SECRET_NAME \
 ParameterKey=KendraWebCrawlerUrl,ParameterValue=$KENDRA_WEBCRAWLER_URL \
 ParameterKey=AmplifyRepository,ParameterValue=$AMPLIFY_REPOSITORY \
 --capabilities CAPABILITY_NAMED_IAM \
---region $AWS_REGION
+--region $AWS_REGION \
+--profile=$AWS_PROFILE
 
-aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION --query "Stacks[0].StackStatus"
-aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region $AWS_REGION
-aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION --query "Stacks[0].StackStatus"
+aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION --query "Stacks[0].StackStatus"  --profile=$AWS_PROFILE
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region $AWS_REGION --profile=$AWS_PROFILE
+aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION --query "Stacks[0].StackStatus"  --profile=$AWS_PROFILE
 
 export LEX_BOT_ID=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`LexBotID`].OutputValue' --output text)
 
 export LAMBDA_ARN=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`LambdaARN`].OutputValue' --output text)
 
-aws lexv2-models update-bot-alias --bot-alias-id 'TSTALIASID' --bot-alias-name 'TestBotAlias' --bot-id $LEX_BOT_ID --bot-version 'DRAFT' --bot-alias-locale-settings "{\"en_US\":{\"enabled\":true,\"codeHookSpecification\":{\"lambdaCodeHook\":{\"codeHookInterfaceVersion\":\"1.0\",\"lambdaARN\":\"${LAMBDA_ARN}\"}}}}" --region $AWS_REGION
+aws lexv2-models update-bot-alias --bot-alias-id 'TSTALIASID' --bot-alias-name 'TestBotAlias' --bot-id $LEX_BOT_ID --bot-version 'DRAFT' --bot-alias-locale-settings "{\"en_US\":{\"enabled\":true,\"codeHookSpecification\":{\"lambdaCodeHook\":{\"codeHookInterfaceVersion\":\"1.0\",\"lambdaARN\":\"${LAMBDA_ARN}\"}}}}" --region $AWS_REGION  --profile=$AWS_PROFILE
 
-aws lexv2-models build-bot-locale --bot-id $LEX_BOT_ID --bot-version "DRAFT" --locale-id "en_US" --region $AWS_REGION
+aws lexv2-models build-bot-locale --bot-id $LEX_BOT_ID --bot-version "DRAFT" --locale-id "en_US" --region $AWS_REGION  --profile=$AWS_PROFILE
 
 export KENDRA_INDEX_ID=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`KendraIndexID`].OutputValue' --output text)
 
 export KENDRA_DATA_SOURCE_ROLE_ARN=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`KendraDataSourceRoleARN`].OutputValue' --output text)
 
 export KENDRA_WEBCRAWLER_DATA_SOURCE_ID=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`KendraWebCrawlerDataSourceID`].OutputValue' --output text)
 
 aws kendra create-faq \
@@ -92,18 +101,21 @@ aws kendra create-faq \
     --s3-path Bucket=$S3_ARTIFACT_BUCKET_NAME,Key="agent/assets/AnyCompany-FAQs.csv" \
     --role-arn $KENDRA_DATA_SOURCE_ROLE_ARN \
     --file-format "CSV_WITH_HEADER" \
-    --region $AWS_REGION
+    --region $AWS_REGION \
+    --profile=$AWS_PROFILE
 
-aws kendra start-data-source-sync-job --id $KENDRA_WEBCRAWLER_DATA_SOURCE_ID --index-id $KENDRA_INDEX_ID --region $AWS_REGION
+aws kendra start-data-source-sync-job --id $KENDRA_WEBCRAWLER_DATA_SOURCE_ID --index-id $KENDRA_INDEX_ID --region $AWS_REGION --profile=$AWS_PROFILE
 
 export AMPLIFY_APP_ID=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`AmplifyAppID`].OutputValue' --output text)
 
 export AMPLIFY_BRANCH=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
     --region $AWS_REGION \
+    --profile=$AWS_PROFILE \
     --query 'Stacks[0].Outputs[?OutputKey==`AmplifyBranch`].OutputValue' --output text)
 
-aws amplify start-job --app-id $AMPLIFY_APP_ID --branch-name $AMPLIFY_BRANCH --job-type 'RELEASE' --region $AWS_REGION
+aws amplify start-job --app-id $AMPLIFY_APP_ID --branch-name $AMPLIFY_BRANCH --job-type 'RELEASE' --region $AWS_REGION --profile=$AWS_PROFILE
